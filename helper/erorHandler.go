6978857d0a2596e38416d3e966f1dd5662298e36
Madumarov Davron <menaceteacher@gmail.com>
2025-04-27 20:47:12 +0500

    Update erorHandler.go

--- helper/erorHandler.go
+++ helper/erorHandler.go
@@ -1,1 +1,12 @@
+package helper
+
+import "github.com/gin-gonic/gin"
+
+// Eror Handler  function
+func ErrorHandler(c *gin.Context, statusCode int, message string) {
+	c.JSON(statusCode, gin.H{
+		"message": message,
+	})
+	return
+}
 
