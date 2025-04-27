f078699750fe18b71af2ff03999ce09347db2c34
Madumarov Davron <menaceteacher@gmail.com>
2025-04-27 20:48:10 +0500

    Update passwordMange

--- helper/passwordMange.go
+++ helper/passwordMange.go
@@ -1,1 +1,24 @@
+package helper
+
+import "golang.org/x/crypto/bcrypt"
+
+// Hash Password
+func HashPassword(password string) (string, error) {
+
+	hashPass, eror := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if eror != nil {
+		return "", eror
+	}
+	return string(hashPass), nil
+}
+
+// Compare Password
+func CompareHashedPassword(password, userPassword string) error {
+
+	eror := bcrypt.CompareHashAndPassword([]byte(userPassword), []byte(password))
+	if eror != nil {
+		return eror
+	}
+	return nil
+}
 
