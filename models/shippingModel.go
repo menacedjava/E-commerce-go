f6657decf890dcd9471ecd3108fe68e0790b3fee
Madumarov Davron <menaceteacher@gmail.com>
2025-04-28 12:43:05 +0500

    Update

--- models/shippingModel.go
+++ models/shippingModel.go
@@ -1,1 +1,30 @@
+package models
+
+import (
+	"context"
+	"github.com/menacedjava/database"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Shipping struct {
+	ID      primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	FName   string             `json:"f_name" bson:"f_name"`
+	LName   string             `json:"l_name" bson:"l_name"`
+	Email   string             `json:"email" bson:"email"`
+	PhoneNo string             `json:"phone_no"`
+	Address string             `json:"address" bson:"address"`
+	State   string             `json:"state" bson:"state"`
+}
+
+func (shipping *Shipping) CreateShipingAddress() (string, error) {
+
+	coll := database.ConnectDB()
+	inserted, err := coll.ShipingAddres.InsertOne(context.TODO(), &shipping)
+
+	if err != nil {
+		return "", err
+	}
+
+	return inserted.InsertedID.(primitive.ObjectID).Hex(), nil
+}
 
