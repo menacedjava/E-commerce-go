1130e10f83a51484b4d43b19102dfdd2140136b6
Madumarov Davron <menaceteacher@gmail.com>
2025-04-29 09:03:42 +0500

    Update

--- routes/productRoutes.go
+++ routes/productRoutes.go
@@ -1,1 +1,17 @@
+package routes
 
+import (
+	"github.com/alijabbar034/controllers"
+	"github.com/alijabbar034/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func ProductRouter(r *gin.RouterGroup) {
+	router := r.Group("/product")
+
+	router.GET("/", controllers.GetAllProduct)
+	router.POST("/", middleware.Authenticate, middleware.Autorize("admin"), controllers.CreateProduct)
+	router.GET("/:id", controllers.GetProductById)
+	router.PUT("/:id", middleware.Authenticate, middleware.Autorize("admin"), controllers.UpdateProduct)
+	router.DELETE("/:id", middleware.Authenticate, middleware.Autorize("admin"), controllers.DeleteProduct)
+}
